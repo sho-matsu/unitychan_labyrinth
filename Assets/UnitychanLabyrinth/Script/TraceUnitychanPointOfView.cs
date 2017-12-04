60420a8f7af987b6aba777acf785c9d8c7a4e5b2
sho <born_in_april_89@yahoo.co.jp>
2017-12-04 17:15:37 +0900

    first commit

---
+++ Assets/UnitychanLabyrinth/Script/TraceUnitychanPointOfView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TraceUnitychanPointOfView : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 targetPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetPos = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ターゲットの移動分、自分も移動する
+        transform.position = new Vector3(target.position.x, target.position.y + target.localScale.y, target.position.z);
+        targetPos = target.position;
+        float horizontal = 0.0f;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            //ゲーム画面に貼り付けたコントローラーからの入力
+            horizontal = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            //// キーボードからの入力
+            horizontal = Input.GetAxis("Horizontal");
+        }
+        // targetの位置のY軸を中心に、回転（公転）する
+        transform.RotateAround(targetPos, Vector3.up, horizontal * Time.deltaTime * 100f);
+    }
+}
---
+++ Assets/UnitychanLabyrinth/Script/WallRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var rotation = transform.rotation;
+		rotation.y += 10;
+		transform.rotation = rotation;
+	}
+}
