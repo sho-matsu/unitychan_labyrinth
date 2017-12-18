edcbf1c4d4bbc1e76763670775e3d98540d56f98
sho <born_in_april_89@yahoo.co.jp>
2017-12-18 18:41:10 +0900

    壁トラップ作成

---
+++ Assets/CreateWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWall : MonoBehaviour {
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        while (target.transform.position.y < 1.2F)
+        {
+            // todo 緩やかにせり出てくるようにしたい
+            target.transform.Translate(Vector3.up * (1.2F * Time.deltaTime), Space.World);
+        }
+        this.gameObject.SetActive(false);
+    }
+}
